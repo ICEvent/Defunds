317854aec760be056ab197a5ec988e69c8c9b7a8
Sun <vansdaynet@gmail.com>
2024-07-11 18:37:42 -0700

    add declarations

---
+++ src/declarations/backend/backend.did.d.ts
@@ -0,0 +1,26 @@
+import type { Principal } from '@dfinity/principal';
+import type { ActorMethod } from '@dfinity/agent';
+import type { IDL } from '@dfinity/candid';
+
+export interface Donation {
+  'txid' : string,
+  'currency' : string,
+  'timestamp' : bigint,
+  'amount' : bigint,
+  'donor' : Principal,
+}
+export type Result = { 'ok' : bigint } |
+  { 'err' : string };
+export type Time = bigint;
+export interface _SERVICE {
+  'donate' : ActorMethod<[bigint, string], Result>,
+  'getDonationHistory' : ActorMethod<
+    [bigint, bigint, [] | [Time], [] | [Time]],
+    Array<Donation>
+  >,
+  'getDonorCredit' : ActorMethod<[string], [] | [bigint]>,
+  'getDonorHistory' : ActorMethod<[Principal, bigint], Array<Donation>>,
+  'updateExchangeRates' : ActorMethod<[string, bigint], Result>,
+}
+export declare const idlFactory: IDL.InterfaceFactory;
+export declare const init: (args: { IDL: typeof IDL }) => IDL.Type[];
---
+++ src/declarations/backend/backend.did.js
@@ -0,0 +1,27 @@
+export const idlFactory = ({ IDL }) => {
+  const Result = IDL.Variant({ 'ok' : IDL.Nat, 'err' : IDL.Text });
+  const Time = IDL.Int;
+  const Donation = IDL.Record({
+    'txid' : IDL.Text,
+    'currency' : IDL.Text,
+    'timestamp' : IDL.Int,
+    'amount' : IDL.Nat,
+    'donor' : IDL.Principal,
+  });
+  return IDL.Service({
+    'donate' : IDL.Func([IDL.Nat, IDL.Text], [Result], []),
+    'getDonationHistory' : IDL.Func(
+        [IDL.Nat, IDL.Nat, IDL.Opt(Time), IDL.Opt(Time)],
+        [IDL.Vec(Donation)],
+        ['query'],
+      ),
+    'getDonorCredit' : IDL.Func([IDL.Text], [IDL.Opt(IDL.Nat)], ['query']),
+    'getDonorHistory' : IDL.Func(
+        [IDL.Principal, IDL.Nat],
+        [IDL.Vec(Donation)],
+        ['query'],
+      ),
+    'updateExchangeRates' : IDL.Func([IDL.Text, IDL.Nat], [Result], []),
+  });
+};
+export const init = ({ IDL }) => { return []; };
